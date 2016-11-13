ca47096d88c3b34f8536d47261e5991bf33aa8ef
ronaldtheodoro <ronaldtheodoro@gmail.com>
2016-11-13 15:48:25 -0200

    Functional test now checks we can input a to-do item

--- apps/lists/tests.py
+++ apps/lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from .views import home_page
 
 
@@ -8,3 +9,10 @@ class HomePageTest(TestCase):
     def test_get_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_redirect(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- apps/lists/views.py
+++ apps/lists/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    pass
+    from django.http import HttpResponse
+    return HttpResponse('<html><title>To-Do lists</title></html>')
