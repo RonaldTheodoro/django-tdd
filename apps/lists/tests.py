4ecde1dd62a26028be943bc41a57e9519017a980
ronaldtheodoro <ronaldtheodoro@gmail.com>
2016-11-14 00:04:00 -0200

    rename HomePageTest tests

--- apps/lists/tests.py
+++ apps/lists/tests.py
@@ -8,12 +8,12 @@ from .models import Item
 
 class HomePageTest(TestCase):
 
-    def test_get_home_page(self):
+    def test_get(self):
         """Check if / is home_page view"""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_contains(self):
+    def test_contains(self):
         """Check if / have title tag"""
         request = HttpRequest()
         response = home_page(request)
@@ -21,20 +21,25 @@ class HomePageTest(TestCase):
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
 
-    def test_home_template(self):
+    def test_template(self):
         """Check if / use index.html"""
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('index.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_post(self):
+    def test_post(self):
         """Check if can save a POST request"""
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'index.html',
