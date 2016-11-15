7e60bfd3099488f5263dc95f2164c2f0e776d12a
ronaldtheodoro <ronaldtheodoro@gmail.com>
2016-11-15 17:48:12 -0200

    update unit tests

--- apps/lists/tests.py
+++ apps/lists/tests.py
@@ -8,27 +8,30 @@ from .models import Item
 
 class HomePageTest(TestCase):
 
-    def test_get(self):
+    def test_root_url_resolves_to_home_page_view(self):
         """Check if / is home_page view"""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_contains(self):
-        """Check if / have title tag"""
-        request = HttpRequest()
-        response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
-
-    def test_template(self):
+    def test_home_page_returns_correct_html(self):
         """Check if / use index.html"""
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('index.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_post(self):
+    def test_home_page_displays_all_list_items(self):
+        """Check if home page display all itens"""
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+    def test_home_page_can_save_a_POST_request(self):
         """Check if can save a POST request"""
         request = HttpRequest()
         request.method = 'POST'
@@ -40,13 +43,22 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'index.html',
-            {'new_item_text': 'A new list item'},
-            request=request
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_redirects_after_POST(self):
+        """Check if home page redirect after POST"""
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_save_items_when_necessary(self):
+        """Check if itens are save only when necessary"""
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
