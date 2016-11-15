2127a425b280e7f3464efed986cdaad1bbcdb047
ronaldtheodoro <ronaldtheodoro@gmail.com>
2016-11-15 17:48:59 -0200

    update views

--- apps/lists/views.py
+++ apps/lists/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Item
 
 
 def home_page(request):
     """Home page"""
-    context = {'new_item_text': request.POST.get('item_text', '')}
-    return render(request, 'index.html', context)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    return render(request, 'index.html', {'items': Item.objects.all()})
